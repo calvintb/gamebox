102b0c15e60017e9edaeddf1e43ecef962b7b386
Braxton Francom <braxtonfrancom@gmail.com>
2023-04-28 10:14:49 -0600

    Made player card with geolocation

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,9 +1,71 @@
+import { PlayerCard } from '../components/PlayerCard';
+import { useEffect, useState } from 'react'
+
 export const Game = () => {
+
+    const [lat, setLat] = useState(0);
+    const [lon, setLon] = useState(0);
+    const [locationLoaded, setLocationLoaded] = useState(false);
+    const [locationData, setLocationData] = useState(null);
+
+    useEffect(() => {
+        const watch = navigator.geolocation.watchPosition((location) => {
+          setLat(location.coords.latitude);
+          setLon(location.coords.longitude);
+          setLocationLoaded(true);
+        }, (err) => {
+          console.log(err)
+        }, {
+          enableHighAccuracy: true,
+        })
+        const fetchAPI = async () => {
+            const url = 'https://api-bdc.net/data/reverse-geocode';
+            const latitude = lat;
+            const longitude = lon;
+            const localityLanguage = 'en';
+            const apiKey = 'bdc_df38e981116f4f8781389060be80ad53';
+      
+            const response = await fetch(`${url}?latitude=${latitude}&longitude=${longitude}&localityLanguage=${localityLanguage}&key=${apiKey}`);
+      
+            if (response.ok) {
+              const jsonData = await response.json();
+              setLocationData(jsonData);
+              console.log(locationData)
+            } else {
+              console.error(`HTTP error! status: ${response.status}`);
+            }
+          };
+          fetchAPI();
+
+        return () => navigator.geolocation.clearWatch(watch)
+      }, []);
 
 
     return(
-        <div>
-            This is the Game Page
-        </div>
+        <main>
+            <div>
+                This is the Game Page
+            </div>
+            
+            <div className='player-card-label'>
+                <PlayerCard name='namegoeshere' geolocation={String(locationLoaded)}/>
+                {/* <PlayerCard name='namegoeshere' geolocation={String(locationData.city)}/>  null error */}
+                <PlayerCard name="John Doe" geolocation="New York" />
+                My location is: {locationLoaded ? (`Lat: ${lat}, Lon: ${lon}`) : 'Loading...'}
+            </div>
+            
+            <div>
+                {locationData && (
+                    <div>
+                    <p>{locationData.city}, {locationData.principalSubdivision}, {locationData.countryCode}</p> 
+                    {/* I need to put a check on the above line of code if a state isnt present  */}
+                    {/* And also it is loading null sometimes. I think i need a wait on my fetch or something...? */}
+                    </div>
+                )}
+            </div>
+
+
+        </main>
+
     )
 }
