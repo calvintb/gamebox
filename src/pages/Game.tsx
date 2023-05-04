e682ca55c86e2759673a5054383b71ffea588b86
calvin.bell <a02296219@usu.edu>
2023-05-03 18:44:11 -0600

    next question

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -100,6 +100,32 @@ export const Game = () => {
     function randNum(min: number, max: number) { // min and max included 
       return Math.floor(Math.random() * (max - min + 1) + min)
     }
+    
+    const nextQuestion = () => {
+        const roomId = location.state.roomId;
+        const refe = ref(database, `/rooms/${roomId}`);
+        let question_index = 0      
+        onValue(refe, (snapshot)=>{
+            question_index = snapshot.val().question;
+
+        })
+        question_index = question_index % 21;
+        update(refe, {
+            seconds:30,
+            question: question_index+1,
+            startAt: serverTimestamp()
+        })
+        setShowResponses(false);
+        const quest = ref(database, `/questions/${room?.question}`)
+      onValue(quest, (snapshot) => {
+        if (snapshot.val()) {
+          const newQuestion = snapshot.val()
+          setQuestion(newQuestion.text);
+        }
+      })
+        startTimer()
+
+    }
 
     useEffect(() => {
       const quest = ref(database, `/questions/${room?.question}`)
@@ -118,11 +144,6 @@ export const Game = () => {
         onValue(refe, (snapshot) => {
             console.log(snapshot)
             timerHasStarted = snapshot.val().seconds != 0
-            if (!timerHasStarted){
-                update(refe, {
-                    startAt: serverTimestamp(),
-                    seconds: 30
-                    });}
             startTimer();
                     
                 })
@@ -257,10 +278,10 @@ export const Game = () => {
                     <h2>HOST: {room.host}</h2> 
                 </>
                 }       
-                <Question prompt={"What food should you not bring to a potluck."}/>
+                <Question prompt={question}/>
                 
                 <input type="text" value={response} onChange={(e)=>setResponse(e.target.value)}/>
-                <button onClick={()=>postResponse()}>SUBMIT RESPONSE</button>
+                <button onClick={()=>{postResponse(); setResponse("")}}>SUBMIT RESPONSE</button>
                 <h1>{timer}</h1>
                 <button>START GAME</button> <button onClick={() => {leaveGame()}}>Leave</button>
                 </>
@@ -282,9 +303,9 @@ export const Game = () => {
                     <h2>HOST: {room.host}</h2> 
                 </>
                 }       
-                <Question prompt={"What food should you not bring to a potluck."}/>
+                <Question prompt={question}/>
                 
-                <button>Next Question</button> <button onClick={() => {leaveGame()}}>Leave</button>
+                <button onClick={()=> nextQuestion()}>Next Question</button> <button onClick={() => {leaveGame()}}>Leave</button>
                 </>
                 {responses.map((response, index) => {
                 return <ResponseCard key={index + response.username} answer={response.response} username={response.username}/>
